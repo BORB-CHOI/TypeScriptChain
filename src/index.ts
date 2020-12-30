89d1f3157ab1b97c76cb758023cf26045553c2ee
Your Name <you@example.com>
2020-12-31 02:35:55 +0900

    Class Example

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,32 @@
-interface Human {
-  age: number;
-  name: string;
-  gender: string;
+class Human {
+  public name: string;
+  private age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender?: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: "bore",
-  gender: "male",
-  age: 19,
-};
+const borb = new Human("BORB", 20, "male");
+
+// interface Human {
+//   age: number;
+//   name: string;
+//   gender: string;
+// }
+
+// const person = {
+//   name: "bore",
+//   gender: "male",
+//   age: 19,
+// };
 
 const sayHi = (InGan: Human): string => {
   return `Hello ${InGan.name}, you are ${InGan.age}, you are a ${InGan.gender}!`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(borb));
 
 export = {};
